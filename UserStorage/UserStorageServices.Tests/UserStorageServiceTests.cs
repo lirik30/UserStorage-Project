658e5627e09461e93924e70474a13a126dc67be2
Kiril Shchasny <kiril3467@yandex.ru>
2017-10-26 23:16:26 +0300

    Add implementation for Add, Remove, Search methods. Add logging

--- UserStorage/UserStorageServices.Tests/UserStorageServiceTests.cs
+++ UserStorage/UserStorageServices.Tests/UserStorageServiceTests.cs
@@ -6,6 +6,7 @@ namespace UserStorageServices.Tests
     [TestClass]
     public class UserStorageServiceTests
     {
+        #region Add Tests
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Add_NullAsUserArgument_ExceptionThrown()
@@ -25,19 +26,124 @@ namespace UserStorageServices.Tests
         {
             // Arrange
             var userStorageService = new UserStorageService();
-            
+
             // Act
             userStorageService.Add(new User
             {
-                FirstName = null
+                FirstName = null,
+                LastName = "name",
+                Age = 1
             });
 
             // Assert - [ExpectedException]
         }
 
         [TestMethod]
-        public void Remove_WithoutArguments_NothingHappen()
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserLastNameIsNull_ExceptionThrown()
         {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                LastName = null,
+                FirstName = "name",
+                Age = 1
+            });
+
+            // Assert - [ExpectedException]
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserAgeIsLessThanZero_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                FirstName = "name",
+                LastName = "name",
+                Age = -10
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        #endregion
+
+        #region Remove Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Remove_NullAsUserArgument_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Remove(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Remove_UserDoesNotExist_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Remove(new User());
+
+            // Assert - [ExpectedException]
+        }
+        #endregion
+        
+        #region Search Tests
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Search_FirstNameIsNull_ExceptionThrows()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByFirstName(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Search_LastNameIsNull_ExceptionThrows()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByLastName(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Search_AgeIsLessThanZero_ExceptionThrows()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByAge(-15);
+
+            // Assert - [ExpectedException]
+        }
+        #endregion
     }
 }
--- UserStorage/UserStorageServices/UserStorageService.cs
+++ UserStorage/UserStorageServices/UserStorageService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 
 namespace UserStorageServices
 {
@@ -8,10 +11,17 @@ namespace UserStorageServices
     public class UserStorageService
     {
         /// <summary>
+        /// User store
+        /// </summary>
+        private HashSet<User> _storage = new HashSet<User>();
+
+        public bool IsLoggingEnabled { get; set; }
+
+        /// <summary>
         /// Gets the number of elements contained in the storage.
         /// </summary>
         /// <returns>An amount of users in the storage.</returns>
-        public int Count { get; }
+        public int Count => _storage.Count;
 
         /// <summary>
         /// Adds a new <see cref="User"/> to the storage.
@@ -19,6 +29,11 @@ namespace UserStorageServices
         /// <param name="user">A new <see cref="User"/> that will be added to the storage.</param>
         public void Add(User user)
         {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("Add() method is called.");
+            }
+
             if (user == null)
             {
                 throw new ArgumentNullException(nameof(user));
@@ -29,23 +44,103 @@ namespace UserStorageServices
                 throw new ArgumentException("FirstName is null or empty or whitespace", nameof(user));
             }
 
-            // TODO: Implement Add() method and all other validation rules.
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                throw new ArgumentException("LastName is null or empty or whitespace", nameof(user));
+            }
+
+            if (user.Age < 0)
+            {
+                throw new ArgumentException("Age is less than zero", nameof(user));
+            }
+
+            user.Id = Guid.NewGuid();
+            _storage.Add(user);
         }
 
         /// <summary>
         /// Removes an existed <see cref="User"/> from the storage.
         /// </summary>
-        public void Remove()
+        public void Remove(User user)
         {
-            // TODO: Implement Remove() method.
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("Remove() method is called.");
+            }
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            var storageContainsUser = _storage.Contains(user);
+
+            if (!storageContainsUser)
+            {
+                throw new InvalidOperationException("There is no record of such user in the storage");
+            }
+
+            _storage.Remove(user);
+        }
+
+        public IEnumerable<User> SearchByFirstName(string firstName)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByFirstName() method is called.");
+            }
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("FirstName is null or empty or whitespace");
+            }
+
+            return Search(user => user.FirstName == firstName);
+        }
+
+        public IEnumerable<User> SearchByLastName(string lastName)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByLastName() method is called.");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException("LastName is null or empty or whitespace");
+            }
+
+            return Search(user => user.LastName == lastName);
+        }
+
+        public IEnumerable<User> SearchByAge(int age)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByAge() method is called.");
+            }
+
+            if (age < 0)
+            {
+                throw new ArgumentException("Age is less than zero");
+            }
+
+            return Search(user => user.Age == age);
         }
 
         /// <summary>
         /// Searches through the storage for a <see cref="User"/> that matches specified criteria.
         /// </summary>
-        public void Search()
+        public IEnumerable<User> Search(Func<User, bool> predicate)
         {
-            // TODO: Implement Search() method.
+            var searchResult = _storage.Where(x => predicate(x));
+
+            if (searchResult.Count() == 0)
+            {
+                // TODO: realize exception when the search result is empty
+            }
+
+            return searchResult;
         }
     }
 }

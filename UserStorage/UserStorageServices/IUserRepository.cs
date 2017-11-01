0b068ec1d6fce64b79bdf4b0d2cfb6a38bd5ab32
Kiril Shchasny <kiril3467@yandex.ru>
2017-11-01 19:42:07 +0300

    Add repository interface

---
+++ UserStorage/UserStorageServices/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStorageServices
+{
+    public interface IUserRepository
+    {
+        void Start();
+
+        void Stop();
+
+        void Get();
+
+        void Set();
+
+        void Query();
+    }
+}
---
+++ UserStorage/UserStorageServices/UserMemoryCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStorageServices
+{
+    public class UserMemoryCache : IUserRepository
+    {
+        private HashSet<User> _storage;
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void Get()
+        {
+
+        }
+
+        public void Set()
+        {
+
+        }
+
+        public void Query()
+        {
+
+        }
+    }
+}

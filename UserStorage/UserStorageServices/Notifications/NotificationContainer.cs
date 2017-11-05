a7e7d8d0d1559a891fa5f5b306afe2e8186803e1
Kiril Shchasny <kiril3467@yandex.ru>
2017-11-06 00:44:04 +0300

    Notification container added

---
+++ UserStorage/UserStorageServices/Notifications/NotificationContainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace UserStorageServices.Notifications
+{
+    [XmlRoot("NotificationContainer", IsNullable = false, Namespace = "http://tempuri.org/userService/notification")]
+    public class NotificationContainer
+    {
+        [XmlArray("notifications")]
+        [XmlArrayItem("notification")]
+        public Notification[] Notifications { get; set; }
+    }
+
+    public class Notification
+    {
+        [XmlIgnore]
+        public NotificationType Type { get; set; }
+
+        [XmlElement("addUser", typeof(AddUserActionNotification))]
+        [XmlElement("deleteUser", typeof(DeleteUserActionNotification))]
+        [XmlChoiceIdentifier("Type")]
+        public object Action { get; set; }
+    }
+
+    [XmlType(IncludeInSchema = false)]
+    public enum NotificationType
+    {
+        [XmlEnum("addUser")]
+        AddUser,
+
+        [XmlEnum("deleteUser")]
+        DeleteUser
+    }
+
+    class DeleteUserActionNotification
+    {
+        [XmlElement("userId")]
+        public int UserId { get; set; }
+    }
+
+    class AddUserActionNotification
+    {
+        [XmlElement("user")]
+        public User User { get; set; }
+    }
+}
